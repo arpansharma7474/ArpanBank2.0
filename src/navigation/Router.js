2071c2d99fe7712b5bd5698f9cc484978e2786e6
Arpan <arpan.futureworks@gmail.com>
2020-02-13 23:00:42 +0530

    SPlash header adjusted

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -1,5 +1,5 @@
 
-
+import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -12,7 +12,12 @@ export default Router = () => {
     const Stack = createStackNavigator()
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Splash">
+            <Stack.Navigator
+                initialRouteName="Splash"
+                screenOptions={{
+                    headerShown: false
+                }}
+            >
                 <Stack.Screen name="Splash" component={SplashScreen} />
                 <Stack.Screen name="Login" component={LoginScreen} />
             </Stack.Navigator>
